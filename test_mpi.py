bbd6f71e8ec7a525f6d2e0fb5647fc7aff93c373
Samuel Pardo <samuelpardo1997@gmail.com>
2025-07-01 17:50:07 -0500

    update test_mpi.py

--- test_mpi.py
+++ test_mpi.py
@@ -1,7 +1,16 @@
 from mpi4py import MPI
+import socket
 
 comm = MPI.COMM_WORLD
 rank = comm.Get_rank()
-size = comm.Get_size()
+hostname = socket.gethostname()
 
-print(f"Hola desde proceso {rank} de {size}", flush=True)
+print(f"Este es el rank {rank} de {comm.Get_size()} ejecutándose en {hostname}")
+
+if rank == 0:
+    data = {"message": "Hola desde el rank 0!"}
+else:
+    data = None
+
+data = comm.bcast(data, root=0)
+print(f"Rank {rank} recibió:", data["message"])
